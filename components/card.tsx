32baffa1c431582046a804566bec3be122c8efbe
Allison Lee <allie.lee0719@gmail.com>
2022-11-04 18:46:04 -0500

    start fixing button

--- components/card.tsx
+++ components/card.tsx
@@ -16,30 +16,28 @@ export const Card = (props: any) => {
             alt=""
           />
         </div>
-        <div className="h-40 bg-white p-6 flex flex-col justify-between">
+        <div className="h-40 bg-white p-6 flex flex-col justify-between relative">
           <p className="text-sm font-medium text-orange-600">
             <a href={props.category.href} className="hover:underline">
               {props.category.name}
             </a>
           </p>
-          <a href={props.href} className="block mt-2">
-            <div>
-              <p className="text-xl font-semibold text-gray-900">
-                {props.title}
-              </p>
-              <div className="flex justify-between">
+            <a href={props.href} className="block mt-2">
+              <div>
+                <p className="text-xl font-semibold text-gray-900">
+                  {props.title}
+                </p>
                 <p className="mt-3 text-base text-gray-500">
                   {props.description}
                 </p>
-                <button
-                  className="py-2 px-3 bg-orange-400 hover:bg-orange-500 rounded font-serif text-gray-200"
-                  onClick={handleImageSelect}
-                >
-                  Add
-                </button>
               </div>
-            </div>
-          </a>
+            </a>
+          <button
+              className="absolute bottom-4 right-4 px-3 py-2 bg-orange-400 hover:bg-orange-500 rounded font-serif text-gray-200"
+              onClick={handleImageSelect}
+          >
+            Add
+          </button>
         </div>
       </div>
     </div>
