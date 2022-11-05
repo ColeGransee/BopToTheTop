4ae32c62c0da3084f4c236cd6844d8360f241c5f
Chanwoong Jhon <cwjhon5654@gmail.com>
2022-11-05 12:59:37 -0500

    updated about page

--- components/about.tsx
+++ components/about.tsx
@@ -1,49 +1,44 @@
-export const AboutHero = () => {
+export const About = () => {
   return (
-    <div className="bg-egg">
-      <div className="text-4xl text-center font-serif font-bold text-gray-900 py-6">
-        About
-      </div>
-      <div className="grid justify-items-center">
-        <div className="flex flex-col px-2 md:px-20">
-          <div className="flex justify-start text-4xl font-serif text-gray-700 font-bold py-4">
-            how to play
-          </div>
-          <div className="flex justify-center text-lg font-serif text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci
-            phasellus egestas tellus rutrum tellus pellentesque eu. Venenatis
-            urna cursus eget nunc scelerisque viverra mauris in. At tellus at
-            urna condimentum mattis pellentesque id nibh. Est ultricies integer
-            quis auctor elit sed. Id neque aliquam vestibulum morbi blandit
-            cursus risus at ultrices. Lorem mollis aliquam ut porttitor leo a
-            diam sollicitudin. Dis parturient montes nascetur ridiculus mus.
-            Faucibus interdum posuere lorem ipsum dolor. Malesuada fames ac
-            turpis egestas maecenas. Lobortis elementum nibh tellus molestie.
-            Porta non pulvinar neque laoreet suspendisse interdum consectetur.
-            Viverra accumsan in nisl nisi scelerisque eu ultrices vitae. In
-            nulla posuere sollicitudin aliquam ultrices sagittis orci. Hendrerit
-            gravida rutrum quisque non tellus orci ac auctor. Turpis egestas
-            pretium aenean pharetra.
-          </div>
-          <div className="flex justify-start text-4xl font-serif text-gray-700 font-bold py-4">
-            the perks
-          </div>
-          <div className="flex justify-center text-lg font-serif text-gray-600">
-            Laoreet suspendisse interdum consectetur libero id. Pretium nibh
-            ipsum consequat nisl vel pretium lectus quam id. Sollicitudin nibh
-            sit amet commodo nulla facilisi nullam. In aliquam sem fringilla ut
-            morbi tincidunt augue interdum velit. Porttitor eget dolor morbi non
-            arcu. Nulla facilisi morbi tempus iaculis urna. Sed tempus urna et
-            pharetra pharetra massa. Ultrices dui sapien eget mi proin sed. Quis
-            risus sed vulputate odio ut enim. Dui vivamus arcu felis bibendum ut
-            tristique et. Massa eget egestas purus viverra accumsan in nisl nisi
-            scelerisque. Quis imperdiet massa tincidunt nunc pulvinar sapien.
-            Velit ut tortor pretium viverra suspendisse potenti nullam. Massa
-            sed elementum tempus egestas sed sed.
+      <div className="bg-egg">
+        {/*<div className="text-4xl text-center font-serif font-bold text-gray-900 py-6">*/}
+        {/*  About*/}
+        {/*</div>*/}
+        <div className="grid justify-items-center py-6">
+          <div className="flex flex-col px-2 md:px-20">
+            <div className="flex justify-start text-4xl font-serif text-gray-700 font-bold py-4">
+              About us
+            </div>
+            <div className="flex justify-center text-lg font-serif text-gray-600">
+              BopTotheTop is a game site focused on selecting clothes featured in the given category of
+              ShopBop's apperal selection. With the image, users select one of each top, bottom, shoe,
+              and accessory based on random assortments of clothing items from Shopbop.
+              ToptotheBop brings recurring value and can create a delightful habit-forming product for the end user,
+              this game will involve creating a recurring way to discover and experience new items on Shopbop.
+              Our goal is to let users discover the latest apparel, activewear, shoes & accessories
+              from established and emerging designers in Shopbop.
+              We appeal to audiences of all ages.
+              We bring laughter and friendship to young people to love to dress up.
+              We also bring memories to older people who reminisce about dressing up dolls when they were young.
+            </div>
+
+            <div className="flex justify-start text-4xl font-serif text-gray-700 font-bold py-4">
+              how to play
+            </div>
+            <div className="flex justify-start text-lg font-serif text-gray-600">
+              1. the program provides the category of the items
+            </div>
+            <div className="flex justify-start text-lg font-serif text-gray-600">
+              2. user selects each top, bottom, shoe and accessory
+            </div>
+            <div className="flex justify-start text-lg font-serif text-gray-600">
+              3. submit the user outfit to the leaderboard
+            </div>
+            <div className="flex justify-start text-lg font-serif text-gray-600">
+              4. user upvote outfits among the leaderboard.
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
