7e68ad01a7809acc1cfdbb0dd2f37f811c95f02e
yashr921 <65732035+yashr921@users.noreply.github.com>
2022-12-01 16:34:17 -0600

    Revert "Ignore"
    This reverts commit ce130bd3ef15fcbc1eed91882a79c146345ae69f.

--- backend/settings.py
+++ backend/settings.py
@@ -86,17 +86,6 @@ DATABASES = {
    }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbtest', 
-        'USER': 'postgres',
-        'PASSWORD': 'capstone',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-    }
-}
-
 
 
 # Password validation
