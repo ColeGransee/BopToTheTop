1c27f9abaff228e56fc3dd56270308a1f8f7119f
Allison Lee <allie.lee0719@gmail.com>
2022-12-06 14:51:50 -0600

    progress

--- components/outfit.tsx
+++ components/outfit.tsx
@@ -13,28 +13,29 @@ export const Outfit = (props: PropsWithChildren<IOutfitProps>) => {
   const selectedTop = props.selectedTop;
   const selectedBottom = props.selectedBottom;
   const selectedAccessory = props.selectedAccessory;
+  let count = 0;
+  let user_submission = [
+    [selectedTop, selectedBottom, selectedAccessory],
+  ];
 
   const handlePost = () => {
     if (props.username) {
       fetch("http://127.0.0.1:8000/submit/", {
         method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'accept': 'application/json',
+          "Content-Type": "application/json",
+          accept: "application/json",
         },
         body: JSON.stringify({
           username: username,
-          top: selectedTop,
-          bottom: selectedBottom,
-          accessory: selectedAccessory,
+          user_submission: [selectedTop, selectedBottom, selectedAccessory],
         }),
       })
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          });
-        }
-     else {
+        });
+    } else {
       router.push("/login");
     }
   };
