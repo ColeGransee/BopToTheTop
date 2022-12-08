45ec0ffa448bc1755887805e32b1f7f779aea422
Allison Lee <88295208+allieklee@users.noreply.github.com>
2022-12-08 12:48:55 -0600

    Issues/27 (#28)
    * added dropdown in header
    
    * added username to signup/login
    
    * finished closet, fixed header in about page

--- routes/about.tsx
+++ routes/about.tsx
@@ -2,12 +2,23 @@ import { Header } from "../components/header";
 import { About } from "../components/about";
 import { Head } from "../components/head";
 import { Footer } from "../components/footer";
+import { useEffect, useState } from "react";
 
 export const AboutPage = () => {
+  const [username, setUsername] = useState<string>();
+
+  useEffect(() => {
+    // Perform localStorage action
+    if (localStorage.getItem("userID") && localStorage.getItem("username")) {
+      // @ts-ignore
+      setUsername(localStorage.getItem("username"));
+    }
+    // empty dependency array means this effect will only run oncfe (like componentDidMount in classes)
+  }, []);
   return (
     <div>
       <Head />
-      <Header />
+      <Header username={username} />
       <About />
       <Footer />
     </div>
--- routes/closet.tsx
+++ routes/closet.tsx
@@ -9,6 +9,9 @@ import { OutfitCard } from "../components/outfitcard";
 export const Closet = () => {
   const [userID, setUserID] = useState<string>();
   const [username, setUsername] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [email, setEmail] = useState<string>();
+
   const [filteredData, setFilteredData] = useState<any[]>([]);
 
   let result = [];
@@ -39,6 +42,10 @@ export const Closet = () => {
       setUserID(localStorage.getItem("userID"));
       // @ts-ignore
       setUsername(localStorage.getItem("username"));
+      // @ts-ignore
+      setEmail(localStorage.getItem("email"));
+      // @ts-ignore
+      setPassword(localStorage.getItem("password"));
     }
     // empty dependency array means this effect will only run oncfe (like componentDidMount in classes)
   }, []);
@@ -62,18 +69,27 @@ export const Closet = () => {
       <Head />
       <Header username={username} />
       <div className="bg-blue-100 p-4 md:px-8 pb-8 border-b-2 border-double border-gray-600">
-        <div className="p-4 font-serif font-bold text-3xl text-gray-900">
+        <div className="p-4 font-serif font-bold text-2xl md:text-3xl  text-gray-900">
           Your Profile
         </div>
-        <div className="flex flex-col p-4 font-serif font-bold text-xl text-gray-700">
-          <div className="">Username: </div>
-          <div className="">Password: </div>
-          <div className="">Email: </div>
+        <div className="flex flex-col p-2 px-6 font-serif text-lg md:text-xl text-gray-900">
+          <div className="p-2 my-2 flex flex-wrap">
+            <div className="font-bold px-2"> Email:</div>
+            <div className=""> {email}</div>
+          </div>
+          <div className="p-2 my-2 flex flex-wrap">
+            <div className="font-bold px-2"> Username:</div>
+            <div className=""> {username}</div>
+          </div>
+          <div className="p-2 my-2 flex flex-wrap">
+            <div className="font-bold px-2"> Password:</div>
+            <div className=""> {password}</div>
+          </div>
         </div>
       </div>
       <div className="bg-pink-100 p-4 md:px-8 pb-8 border-b-2 border-double border-gray-600">
-        <div className="p-4 font-serif font-bold text-3xl text-gray-900">
-          {username}'s fits
+        <div className="p-4 font-serif font-bold text-2xl md:text-3xl text-gray-900 overflow-x-auto">
+          Your fits
         </div>
         <div className="">
           <Carousel
